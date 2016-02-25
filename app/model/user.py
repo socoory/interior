1fb15c76d31bbfe827b76f7bd903dc86ccd14afc
Mocery <benimario@coupang.com>
2016-02-25 19:30:34 +0900

    create auction entity

--- app/model/user.py
+++ app/model/user.py
@@ -5,10 +5,11 @@ from ..database import Base
 class User(Base):
     __tablename__ = 'user'
 
-    user_sql = Column(Integer, primary_key=True, autoincrement=True)
+    user_srl = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(64), unique=True, index=True)
-    password = Column(String(64))
-    name = Column(String(32))
+    password = Column(String(64), nullable=True)
+    name = Column(String(32), nullable=True)
+    user_type = Column(String(15), nullable=False)
     regdate = Column(DateTime)
 
     def __init__(self):
