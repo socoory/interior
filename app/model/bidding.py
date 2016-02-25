f53cf6122aac3d2e82da4943f58af3d611cd2b92
BigMac(youngkyo) <ykyo.jung@coupang.com>
2016-02-25 20:07:36 +0900

    Bidding Class Create

--- app/model/bidding.py
+++ app/model/bidding.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 from ..database import Base
 
@@ -6,6 +7,8 @@ class Bidding(Base):
     __tablename__ = "bidding"
 
     bidding_url = Column(Integer, primary_key=True, autoincrement=True)
+
+    auction = relationship('Auction', ForeignKey('auction.auction_srl'))
 
     def __init__(self):
         pass
--- run.py
+++ run.py
@@ -1,2 +1,6 @@
 from app.app import app
+from app.database import init
+
+init()
+
 app.run(debug=True)
